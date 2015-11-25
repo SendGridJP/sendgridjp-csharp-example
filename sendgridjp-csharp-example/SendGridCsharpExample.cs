97240b4d884ef38e92efc4b2bb2821f9406b56f8
Wataru Sato <awwa500@gmail.com>
2015-11-25 16:45:00 +0900

    Update for sendgrid-csharp version 6.3.0

--- sendgridjp-csharp-example/SendGridCsharpExample.cs
+++ sendgridjp-csharp-example/SendGridCsharpExample.cs
@@ -14,31 +14,26 @@ namespace sendgridjp_csharp_example
     {
         static void Main(string[] args)
         {
-            String sendGridUserName = ConfigurationManager.AppSettings["SENDGRID_USERNAME"];
-            String sendGridPassword = ConfigurationManager.AppSettings["SENDGRID_PASSWORD"];
+            String apiKey = ConfigurationManager.AppSettings["API_KEY"];
             List<String> tos = new List<String>(ConfigurationManager.AppSettings["TOS"].Split(','));
             String from = ConfigurationManager.AppSettings["FROM"];
 
-            var smtpapi = new SendGrid.SmtpApi.Header();
-            smtpapi.SetTo(tos); // 宛先はこちらで指定したものが使用される
-            smtpapi.AddSubstitution("fullname", new List<String>() { "田中 太郎", "佐藤 次郎", "鈴木 三郎" });
-            smtpapi.AddSubstitution("familyname", new List<String>() { "田中", "佐藤", "鈴木" });
-            smtpapi.AddSubstitution("place", new List<String>() { "office", "home", "office" });
-            smtpapi.AddSection("office", "中野");
-            smtpapi.AddSection("home", "目黒");
-            smtpapi.SetCategory("category1");
-
             var email = new SendGrid.SendGridMessage();
-            email.AddTo(from);  // 実際にはこのアドレスには送信されない。エラー回避のため記載。
+            email.AddTo(from);          // 実際にはこのアドレスには送信されない。エラー回避のため記載。
+            email.Header.SetTo(tos);    // 宛先はこちらで指定したものが使用される
             email.From = new MailAddress(from, "送信者名");
             email.Subject = "[sendgrid-c#-example] フクロウのお名前はfullnameさん";
             email.Text = "familyname さんは何をしていますか？\r\n 彼はplaceにいます。";
             email.Html = "<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。";
-            email.Headers.Add("X-Smtpapi", smtpapi.JsonString());
+            email.AddSubstitution("fullname", new List<String>() { "田中 太郎", "佐藤 次郎", "鈴木 三郎" });
+            email.AddSubstitution("familyname", new List<String>() { "田中", "佐藤", "鈴木" });
+            email.AddSubstitution("place", new List<String>() { "office", "home", "office" });
+            email.AddSection("office", "中野");
+            email.AddSection("home", "目黒");
+            email.SetCategory("category1");
             email.AddAttachment(@"..\..\gif.gif");
 
-            var credentials = new NetworkCredential(sendGridUserName, sendGridPassword);
-            var web = new Web(credentials);
+            var web = new Web(apiKey);
             var task = web.DeliverAsync(email);
             task.Wait();
         }
